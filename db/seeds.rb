9bc8436322262794a086e4a5451de217096b8f6e
Alvin <peng.edy@gmail.com>
2014-10-09 01:42:22 +0800

    second seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first
+# Generate Author
 Author.create({name: 'Qian Chen'})
 Author.create({name: 'B.B.'})
+puts "Author 'Qian Chen' and 'B.B.' generated."
 
+# Generate Category
 Category.create({name: 'Scenery'})
 Category.create({name: 'Fashion'})
 Category.create({name: 'Wedding'})
 Category.create({name: 'Portrait'})
 Category.create({name: 'Animal'})
+puts "5 categories generated."
+
+# Generate Picture
+
+# B.B. Pictures
+23.times do |n|
+  pic = Picture.new
+  pic.name = ""
+  pic.category_id = rand(1..5)
+  pic.description = ""
+  pic.author_id = 2
+  pic.photo = File.open("#{Rails.root}/public/develop_static/BB/#{n+1}.jpg")
+  pic.save
+  puts "#{n+1} photo saved."
+end
+puts "Pictures of B.B. generated."
+
+# Qian Chen Picture
+26.times do |n|
+  pic = Picture.new
+  pic.name = ""
+  pic.category_id = rand(1..5)
+  pic.description = ""
+  pic.author_id = 1
+  pic.photo = File.open("#{Rails.root}/public/develop_static/CQ/#{n+1}.jpg")
+  pic.save
+  puts "#{n+1} photo saved."
+end
+puts "Pictures of B.B generated."
