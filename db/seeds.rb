7eabc096b20b61453e7fb04652986416ee4a7ef4
Alvin <peng.edy@gmail.com>
2014-10-10 02:56:35 +0800

    rollback seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,27 +14,27 @@ puts "5 categories generated."
 # Generate Picture
 
 # B.B. Pictures
-23.times do |n|
-  pic = Picture.new
-  pic.name = "BB#{n+1}"
-  pic.category_id = rand(1..5)
-  pic.description = ""
-  pic.author_id = 2
-  pic.photo = File.open("#{Rails.root}/public/develop_static/BB/#{n+1}.jpg")
-  pic.save!
-  puts "#{n+1} photo saved as B.B."
-end
-puts "Pictures of B.B. generated."
+# 23.times do |n|
+#   pic = Picture.new
+#   pic.name = "BB#{n+1}"
+#   pic.category_id = rand(1..5)
+#   pic.description = ""
+#   pic.author_id = 2
+#   pic.photo = File.open("#{Rails.root}/public/develop_static/BB/#{n+1}.jpg")
+#   pic.save!
+#   puts "#{n+1} photo saved as B.B."
+# end
+# puts "Pictures of B.B. generated."
 
 # Qian Chen Picture
-26.times do |n|
-  pic = Picture.new
-  pic.name = "CQ#{n+1}"
-  pic.category_id = rand(1..5)
-  pic.description = ""
-  pic.author_id = 1
-  pic.photo = File.open("#{Rails.root}/public/develop_static/CQ/#{n+1}.jpg")
-  pic.save!
-  puts "#{n+1} photo saved as CQ."
-end
-puts "Pictures of B.B generated."
+# 26.times do |n|
+#   pic = Picture.new
+#   pic.name = "CQ#{n+1}"
+#   pic.category_id = rand(1..5)
+#   pic.description = ""
+#   pic.author_id = 1
+#   pic.photo = File.open("#{Rails.root}/public/develop_static/CQ/#{n+1}.jpg")
+#   pic.save!
+#   puts "#{n+1} photo saved as CQ."
+# end
+# puts "Pictures of B.B generated."
