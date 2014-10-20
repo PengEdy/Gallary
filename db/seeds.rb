dbb88662f9e7e35a15dc53db8db7d87653b6c6d1
Alvin <peng.edy@gmail.com>
2014-10-20 15:29:21 +0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,16 @@ Author.create({name: 'B.B.'})
 puts "Author 'Qian Chen' and 'B.B.' generated."
 
 # Generate Category
-Category.create({name: 'Scenery'})
-Category.create({name: 'Fashion'})
+Category.create({name: 'Pre-wedding'})
 Category.create({name: 'Wedding'})
+Category.create({name: 'Maternity'})
+Category.create({name: 'Baby&family'})
 Category.create({name: 'Portrait'})
-Category.create({name: 'Animal'})
-puts "5 categories generated."
+Category.create({name: 'Commercial'})
+Category.create({name: 'Service & Price'})
+Category.create({name: 'Promotion'})
+
+puts "8 categories generated."
 
 # Generate Picture
 
@@ -17,7 +21,7 @@ puts "5 categories generated."
  23.times do |n|
    pic = Picture.new
    pic.name = "BB#{n+1}"
-   pic.category_id = rand(1..5)
+   pic.category_id = rand(1..8)
    pic.description = ""
    pic.author_id = 2
    pic.photo = File.open("#{Rails.root}/public/develop_static/BB/#{n+1}.jpg")
@@ -30,7 +34,7 @@ puts "5 categories generated."
  26.times do |n|
    pic = Picture.new
    pic.name = "CQ#{n+1}"
-   pic.category_id = rand(1..5)
+   pic.category_id = rand(1..8)
    pic.description = ""
    pic.author_id = 1
    pic.photo = File.open("#{Rails.root}/public/develop_static/CQ/#{n+1}.jpg")
