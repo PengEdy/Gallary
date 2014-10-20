01e577ded485d51c86f2aca53d11fe99c705dfa2
Alvin <peng.edy@gmail.com>
2014-10-20 20:38:58 +0800

    signed_in_user unfinished

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
   before_action :set_author, only: [:show, :edit, :update, :destroy]
+  before_action :signed_in_user, only: [:edit, :update, :create, :destroy]
 
   # GET /authors
   # GET /authors.json
