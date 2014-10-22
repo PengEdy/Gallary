279d32ff3f2e171997f75240b641004fd46d0338
Alvin <peng.edy@gmail.com>
2014-10-22 13:26:18 +0800

    update cap config

--- Capfile
+++ Capfile
@@ -22,23 +22,3 @@ require 'capistrano/rails/assets'
 require 'capistrano/rails/migrations'
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
 Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
-
-set :rvm_type, :user
-set :rvm_ruby_version, '2.1.3-p242'
-
-set :application, 'Gallary'
-set :repo_url, 'https://github.com/PengEdy/Gallary.git'
-set :deploy_to, 'home/deploy/Gallary'
-set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/budnle public/system public/develop_static}
-namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequance, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, 'deploy:restart'
-  after :finishing, 'deploy:cleanup'
-end
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.1.0'
 
 set :application, 'Gallary'
-set :repo_url, 'git@github.com:PengEdy/Gallary.git'
+set :repo_url, 'https://github.com/PengEdy/Gallary.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
@@ -34,25 +34,20 @@ set :repo_url, 'git@github.com:PengEdy/Gallary.git'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
+set :rvm_type, :user
+set :rvm_ruby_version, '2.1.3-p242'
 
+set :deploy_to, 'home/deploy/Gallary'
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/budnle public/system public/develop_static}
+namespace :deploy do
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+    on roles(:app), in: :sequance, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
 end
